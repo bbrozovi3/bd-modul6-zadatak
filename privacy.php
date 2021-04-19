a10714ea78ead5f2a7a850fdcdc9301bf0b67810
Bruno Brozović <brunobrozovic1976@gmail.com>
2021-04-19 20:53:06 +0200

    Dodan meni na sve stranice-popravljen error

--- privacy.php
+++ privacy.php
@@ -4,11 +4,6 @@
   <a href="privacy.php">Privacy</a>
   <a href="about.php">About</a>
 </div>
-<div class="topnav">
-  <a class="active" href="index.php">Home</a>
-  <a href="privacy.php">Privacy</a>
-  <a href="about.php">About</a>
-</div>
 <br>
 <hr>
 <h2>This is privacy page</h2>
