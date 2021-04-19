ac0f710f4805bb07d4e14277d8e462556fe404f4
Bruno Brozović <brunobrozovic1976@gmail.com>
2021-04-19 20:20:11 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="privacy.php">Privacy</a>
+  <a href="about.php">About</a>
+</div>
+<br>
+<hr>
+<h2>This is home page</h2>
